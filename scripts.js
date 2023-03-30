ebdae00059f266708194744cef6068b531430da7
thallesnog <thallemmanuel@gmail.com>
2023-03-30 16:06:11 -0300

    Projeto feito durante as lives do Rodolfo Mori

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaimg(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+}
